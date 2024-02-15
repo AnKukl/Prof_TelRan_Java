3ff8d2997c514c7030549137181dcdeae8368af6
Anton.Kuklinski <kuklinskijanton@gmail.com>
2024-02-15 10:31:59 +0100

    Java Practise in february

--- src/main/java/com/proftelran/org/lessontwentyfive/TestSelfWork.java
+++ src/main/java/com/proftelran/org/lessontwentyfive/TestSelfWork.java
@@ -1,4 +1,16 @@
 package com.proftelran.org.lessontwentyfive;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class TestSelfWork {
+    public static void main(String[] args) {
+
+        boolean b = checkYear(2024);
+        System.out.println("Is 2024 leap year: " + b);
+    }
+
+    private static boolean checkYear(int year){
+        return new GregorianCalendar().isLeapYear(year);
+    }
 }
